4fc2e1c9c3f505d6670f065f8dad6c24202d6775
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-01-26 18:10:46 +0530

    Refactor Login component to enhance structure and navigation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+import { useRouter } from "expo-router";
 
-const login = () => {
+const Login = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>login</Text>
+    <View className="flex-1 justify-center items-center bg-slate-50">
+      <Text className="text-3xl font-bold mb-6">Login Page</Text>
+      <Pressable
+        className="bg-blue-600 py-3 px-6 rounded"
+        onPress={() => router.push("/(dashboard)/home")}
+      >
+        <Text className="text-white font-bold">Go to Dashboard</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default login
+export default Login;
