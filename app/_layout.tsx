713586c3ef59a5ad1fe16e710cc34dbfff1b7867
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-01-26 18:41:36 +0530

    Wrap RootLayout in Redux Provider for state management

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import { Slot } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native";
+import { store } from "@/src/redux/store";
+import { Provider } from "react-redux";
 
 const RootLayout = () => {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView className="flex-1 bg-white">
-        <View className="flex-1">
-          <Slot />
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <SafeAreaView className="flex-1 bg-white">
+          <View className="flex-1">
+            <Slot />
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
