d632fcc9a477f112c34f0c4d35d55d75bf200b9a
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-01-26 20:22:56 +0530

    Add user authentication state management in RootLayout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,29 @@ import { Slot } from "expo-router"
 import { Provider } from "react-redux"
 import { store } from "@/src/redux/store"
 import { SafeAreaProvider } from "react-native-safe-area-context"
+import { useEffect } from 'react'
+import { subscribeToAuthChanges } from '@/src/services/authService'
+import { setUser } from '@/src/redux/slices/authSlice'
+import { useAppDispatch } from '@/src/hooks/useAppDispatch'
 
 export default function RootLayout() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const unsubscribe = subscribeToAuthChanges(user => {
+      if (user) {
+        dispatch(setUser({
+          uid: user.uid,
+          email: user.email,
+          name: user.displayName
+        }))
+      } else {
+        dispatch(setUser(null))
+      }
+    })
+    return () => unsubscribe()
+  }, [dispatch])
+  
   return (
     <SafeAreaProvider>
       <Provider store={store}>
