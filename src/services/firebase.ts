830f7e290976fc03a8a2c51abbcfe22a8af417c3
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-01-28 06:14:32 +0530

    Refactor Firebase configuration to use environment variables for sensitive data

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -12,19 +12,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  // authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 
-  apiKey: "AIzaSyAbpyHGXbs0Pmx2psosV7XH_Reo0UebsQ0",
-  authDomain: "aurora-mobile-app-45fdf.firebaseapp.com",
-  projectId: "aurora-mobile-app-45fdf",
-  storageBucket: "aurora-mobile-app-45fdf.firebasestorage.app",
-  messagingSenderId: "92143689617",
-  appId: "1:92143689617:web:c98b1994094ebb21854430",
 };
 
 // Initialize Firebase
