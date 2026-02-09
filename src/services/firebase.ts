60683ce3f8d6de55608abdc5b78404f7df470e4f
GamithaGimhana <gamitha.gimhana99@gmail.com>
2026-02-09 22:42:25 +0530

    Add internet permissions, update Firebase configuration, and bump brace-expansion version

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -12,13 +12,21 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAbpyHGXbs0Pmx2psosV7XH_Reo0UebsQ0",
+  authDomain: "aurora-mobile-app-45fdf.firebaseapp.com",
+  projectId: "aurora-mobile-app-45fdf",
+  storageBucket: "aurora-mobile-app-45fdf.firebasestorage.app",
+  messagingSenderId: "92143689617",
+  appId: "1:92143689617:web:c98b1994094ebb21854430",
 };
+// const firebaseConfig = {
+//   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+// };
 
 // Initialize Firebase
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
